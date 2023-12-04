da6e1a0140e1489c7e40c8b1c4438acc5c478e9e
Nigel Robinson <nrobinso@made-f0Cr6s1.clusters.42paris.fr>
2023-12-04 14:58:24 +0100

    read works with file

---
+++ src/get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 09:29:01 by nrobinso          #+#    #+#             */
+/*   Updated: 2023/12/04 14:07:28 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd)
+{
+	
+	char *ptr;
+
+	ptr = malloc(5 * sizeof(char));
+	if (!ptr)
+		return (0);
+
+
+	read(fd, ptr, 5); 
+
+	return (ptr);
+
+}
---
+++ src/test_get_next_line.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_get_next_line.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 09:30:46 by nrobinso          #+#    #+#             */
+/*   Updated: 2023/12/04 14:57:22 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int fd;	
+	char *ptr;	
+
+	fd = open("text.txt", O_RDONLY);
+	if (!fd)
+		return (write(1, "error", 5));
+
+	ptr = get_next_line(fd);
+	printf("%s", ptr);
+
+	while (*ptr != '\0')
+	{
+		ptr = get_next_line(fd);
+		printf("%s", ptr);
+		ptr++;
+	}
+
+	return (0);
+}
